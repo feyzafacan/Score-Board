175ea29cc7c6fc49bd23d1a5a90ba3dfdd155ffa
feyzafacan <104005289+feyzafacan@users.noreply.github.com>
2022-10-02 16:11:35 +0200

    Add score board files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+let homeScoreEl = document.getElementById("homescore");
+let guestScoreEl = document.getElementById("guestscore");
+let homeScore = 0;
+let guestScore = 0;
+
+
+function reset() {
+    homeScore = 0
+    guestScore = 0
+    homeScoreEl.textContent = homeScore
+    guestScoreEl.textContent = guestScore
+}
+
+function increment_home1() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+function increment_home2() {
+    increment_home1()
+    increment_home1()
+}
+function increment_home3() {
+    increment_home1()
+    increment_home1()
+    increment_home1()
+}
+function increment_guest1() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+function increment_guest2() {
+    increment_guest1()
+    increment_guest1()
+}
+function increment_guest3() {
+    increment_guest1()
+    increment_guest1()
+    increment_guest1()
+}
